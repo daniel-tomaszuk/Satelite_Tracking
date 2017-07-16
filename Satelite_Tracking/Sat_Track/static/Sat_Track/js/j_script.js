1bf840fa66042bdd5a0ca96d5a55d04734dd633b
Daniel Tomaszuk <dtomaszuk01@gmail.com>
2017-07-16 14:02:01 +0200

    Add js cookies maintenance stript

--- Satelite_Tracking/Sat_Track/static/Sat_Track/js/j_script.js
+++ Satelite_Tracking/Sat_Track/static/Sat_Track/js/j_script.js
@@ -1,50 +1,5 @@
 var URL = 'http://localhost:8000/history/';
 
-
-// one ajax to rule them all
-var modifyDB = function(myType, myData, urlAddon, successFunction){
-/**
- * ajax function for DB modifications.
- * @myType {string} myType - type of action that ajax should do: GET, POST, DELETE
- * @myData {string} myData  - json data to send, only for POST action
- * urlAddon {string} urlAddon - string with id of item in DB to make action on
- * @successFunction {object} successFunction - object - function to fire when ajax successfully finish given action
- */
-    $.ajax({
-        url: URL + urlAddon,
-        data: myData,
-        type: myType,
-        crossDomain: true,
-        dataType: 'json',
-        async: false, // TO DO: working with true
-        success: function(response){
-            successFunction(response);
-        },
-        error: function(response){
-            console.log('Ajax Fail!\n' + response);
-        }
-    });
-};
-
-// ajax success functions
-var ajaxGet = function(response){
-/**
-* success function for ajax GET action
-* @response {string} response - json with DB server response
-*/
-    console.log('Success data download!');
-};
-
-// get books from DB
-// GET data from DB by means of asynchronous ways ..
-// 18ms < 50%; 25ms -> 100% for localhost
-var getSatHistory = function(urlAddon){
-    modifyDB('GET', '{}', urlAddon, ajaxGet); // passing ajaxGet as object
-    // with function inside
-};
-
-
-
 var scaleX = function(lati, xMax){
     /**
     * Scale geographical latitude into x [px] coordinate.
@@ -54,7 +9,6 @@ var scaleX = function(lati, xMax){
     var xPx = (xMax / 360) * lati + 0.5 * xMax;
     return xPx
 };
-
 
 var scaleY = function(longi, yMax){
     /**
@@ -66,14 +20,13 @@ var scaleY = function(longi, yMax){
     return yPx;
 };
 
-
-var divPoint = function(size, color){
+var mapDivPoint = function(size, color){
     /**
-    * Create point as DIV tag.
+    * Create point as mapDiv tag.
     * @param {string} size - size of the point. Same as in CSS.
     * @param {string} color - Color of the point. Same as in CSS.
     */
-    var point = $('<div>');
+    var point = $('<mapDiv>');
     var size = '3px';
     $(point)
         .css('position', 'absolute')
@@ -82,7 +35,6 @@ var divPoint = function(size, color){
         .css('background-color', color);
     return $(point)
 };
-
 
 var createLineElement = function(x, y, length, angle, color){
     /**
@@ -93,7 +45,7 @@ var createLineElement = function(x, y, length, angle, color){
     * @param {number} angle - Angle of the line.
     * @param {string} color - Color of the line.
     */
-    var line = document.createElement("div");
+    var line = document.createElement('div');
     var styles = 'border: 1px solid;'
                + 'width: ' + length + 'px; '
                + 'height: 0px; '
@@ -108,7 +60,6 @@ var createLineElement = function(x, y, length, angle, color){
     line.setAttribute('style', styles);
     return line;
 };
-
 
 var createLine = function(x1, y1, x2, y2, color) {
     /**
@@ -136,72 +87,123 @@ var createLine = function(x1, y1, x2, y2, color) {
 };
 
 
-document.addEventListener("DOMContentLoaded", function(){
-    var div = document.querySelector('#box');
-    // get dimensions of map image
-    var positionInfo = div.getBoundingClientRect();
+
+// one ajax to rule them all
+var modifyDB = function(myType, myData, urlAddon, successFunction){
+/**
+ * ajax function for DB modifications.
+ * @myType {string} myType - type of action that ajax should do: GET, POST,
+                             DELETE
+ * @myData {string} myData  - json data to send, only for POST action
+ * @urlAddon {string} urlAddon - string which will be added to standard URL
+ * @mapDiv {object} mapDiv - map div tag from map.html
+ * @successFunction {object} successFunction - function to fire when ajax
+                                               successfully finish given action
+ */
+    $.ajax({
+        url: URL + urlAddon,
+        data: myData, // data to send to the server
+        type: myType, // GET, POST, DELETE, PUT
+        crossDomain: false,
+        dataType: 'json',
+        async: true, // TO DO: working with true
+        success: function(response){
+            successFunction(response);  // function to fire when success
+        },
+        error: function(response){
+            console.log('Ajax Fail!\n' + response);
+        }
+    });
+};
+
+// ajax success functions
+var ajaxGet = function(response){
+/**
+* success function for ajax GET action
+* @response {string} response - json with DB server response
+*/
+    var mapDiv = document.querySelector('#box');
+    console.log('Success data download!');
+    var positionInfo = mapDiv.getBoundingClientRect();
+
+    // find how many unique satellite names there is in response
+    var unique_names = [];
+    for (var i = 0; i < response.length; i++){
+        // if no name in unique names list, add name to list
+        if (!(unique_names.includes(response[i].name))){
+            unique_names.push(response[i].name);
+        };
+    };
+
+    console.log('unique names')
+    console.log(unique_names);
+
+
+
+
+
     // loop through actual positions
-    // sats_json from map view in django
+    // sats_json from base view in django
     // main loop for every chosen satellite
-    for (var i = 0; i < sats_json.length; i++){
+    for (var i = 0; i < unique_names.length; i++){
         var point_list = [];
         // past data points color
         var color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
 
-        var satIcon = $('<img />');
-        var dataContent = "Longi: "+ (sats_json[i].fields.longi).toFixed(2) +
-                          '<br>' +
-                          "Lati: " + (sats_json[i].fields.lati).toFixed(2) +
-                          '<br>' +
-                          "Alti: " + (sats_json[i].fields.alti.toFixed(2))
-
-        $(satIcon).attr('class', 'pop');
-        $(satIcon).attr('id', 'SatId_' + i);
-        $(satIcon).attr('src', 'static/Sat_Track/sat.png');
-        $(satIcon).attr('alt', 'Sat' + i);
-        $(satIcon).attr('data-toggle', 'popover');
-        $(satIcon).attr('data-trigger', 'focus');
-        $(satIcon).attr('data-html', true);
-        $(satIcon).attr('title', sats_json[i].fields.name);
-        $(satIcon).attr('data-content', dataContent);
-        $(satIcon).css('display','none');
-        $(satIcon).css('width', "2%");
-        $(satIcon).css('height', "4%");
-        $(satIcon).css('position', 'absolute');
-        $(satIcon).css('top', '0px');
-        $(satIcon).css('left', '0px');
-
-        // put the most actual satellite position on map
-        // count where to put the most actual satellite position
-        xPx = scaleX(sats_json[i].fields.longi, positionInfo.width);
-        yPx = scaleY(sats_json[i].fields.lati, positionInfo.height);
-        $(satIcon).css('left', xPx - 10 + "px");
-        $(satIcon).css('top', yPx - 20 + "px");
-        $(satIcon).css('display', '');
-        $(satIcon).css('z-index', '10');
-        $(satIcon).appendTo($('#box'));
+//        var satIcon = $('<img />');
+//        var dataContent = "Longi: "+ (sats_json[i].fields.longi).toFixed(2) +
+//                          '<br>' +
+//                          "Lati: " + (sats_json[i].fields.lati).toFixed(2) +
+//                          '<br>' +
+//                          "Alti: " + (sats_json[i].fields.alti.toFixed(2))
+//
+//        $(satIcon).attr('class', 'pop');
+//        $(satIcon).attr('id', 'SatId_' + i);
+//        $(satIcon).attr('src', 'static/Sat_Track/sat.png');
+//        $(satIcon).attr('alt', 'Sat' + i);
+//        $(satIcon).attr('data-toggle', 'popover');
+//        $(satIcon).attr('data-trigger', 'focus');
+//        $(satIcon).attr('data-html', true);
+//        $(satIcon).attr('title', sats_json[i].fields.name);
+//        $(satIcon).attr('data-content', dataContent);
+//        $(satIcon).css('display','none');
+//        $(satIcon).css('width', "2%");
+//        $(satIcon).css('height', "4%");
+//        $(satIcon).css('position', 'absolute');
+//        $(satIcon).css('top', '0px');
+//        $(satIcon).css('left', '0px');
+//
+//        // put the most actual satellite position on map
+//        // count where to put the most actual satellite position
+//        xPx = scaleX(sats_json[i].fields.longi, positionInfo.width);
+//        yPx = scaleY(sats_json[i].fields.lati, positionInfo.height);
+//        $(satIcon).css('left', xPx - 10 + "px");
+//        $(satIcon).css('top', yPx - 20 + "px");
+//        $(satIcon).css('display', '');
+//        $(satIcon).css('z-index', '10');
+//        $(satIcon).appendTo($('#box'));
 
         // history points of i-th satellite
-        for (var j=0; j < sats_hist_json.length; j++){
-        // find history objects for actual object (i-th object)
-            if (sats_hist_json[j].fields.name === sats_json[i].fields.name){
-                var point = divPoint('3px', color);
+        for (var j = 0; j < response.length; j++){
+        // find history objects for satellite with name i-th unique name
+            if (response[j].name === unique_names[i]){
+                var point = mapDivPoint('3px', color);
                 $(point).appendTo($('#box'));
                 // count where to put past position icon
-                var xPx = scaleX(sats_hist_json[j].fields.longi,
+                var xPx = scaleX(response[j].longi,
                                  positionInfo.width);
-                var yPx = scaleY(sats_hist_json[j].fields.lati,
+                var yPx = scaleY(response[j].lati,
                                  positionInfo.height);
                 // put past position icon
-                $(point).css('left', xPx+"px");
-                $(point).css('top', yPx+"px");
+                $(point).css('left', xPx + "px");
+                $(point).css('top', yPx + "px");
                 $(point).css('display', '');
                 // push history x and y coordinates in the coordinates list
                 point_list.push([xPx, yPx]);
             };
-    };
+        };
 
-        for (var k=0; k < parseInt((point_list.length)) - 1; k++){
+        for (var k =0 ; k < parseInt((point_list.length)) - 1; k++){
         //console.log(point_list[k][0]);
 
             var x1 = parseInt(point_list[k][0])
@@ -213,19 +215,20 @@ document.addEventListener("DOMContentLoaded", function(){
                                  Math.pow((y1 - y2), 2));
             // draw line if the distance is close enough
             if (dist < 100){
-                div.appendChild(createLine(x1, y1, x2, y2, color));
+                mapDiv.appendChild(createLine(x1, y1, x2, y2, color));
             };
         };
 
         // legend
         var legend = $('#legend');
         var legendText = $('<p>');
-        $(legendText).text(sats_json[i].fields.name + " ");
+        $(legendText).text(unique_names[i] + " ");
         $(legendText).css('color', color);
         $(legendText).css('font-weight', 'bold');
         $(legendText).css('display', 'inline');
         $(legendText).appendTo($(legend));
     };
+
     // popovers
     $(".pop").each(function() {
         $(this).on('mouseover', function(event){
@@ -240,20 +243,73 @@ document.addEventListener("DOMContentLoaded", function(){
             $(document).off('click.popover');
         });
     });
+};
+// get books from DB
+// GET data from DB by means of asynchronous ways ..
+// 18ms < 50%; 25ms -> 100% for localhost
 
-    var globalX = document.querySelector('#globalX');
-    var globalY = document.querySelector('#globalY');
-    var localX = document.querySelector('#localX');
-    var localY = document.querySelector('#localY');
 
-    div.addEventListener('mousemove', function(event){
-       localX.innerHTML = event.clientX - div.offsetLeft;
-       localY.innerHTML = event.clientY - div.offsetTop;
+
+var getSatHistory = function(urlAddon){
+    modifyDB('GET', '{}', urlAddon, ajaxGet);
+};
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    var urlAddon = '';
+    var mapDiv = document.querySelector('#box');
+    var satForm = $('#satellite_form');
+    $(satForm).on('submit', function(event){
+        // get required information from satellites.html for API GET prepare
+//         event.preventDefault();
+        // var for GET request
+        var selectDate = $('#select_date');
+        var selectSat = $(':checkbox');
+        var satCheckList = [];
+        urlAddon = '';
+        $(selectSat).each(function(){
+            if ($(this).prop('checked')){
+                satCheckList.push(($(this).val()));
+            };
+        });
+        console.log($(selectDate).val())
+        console.log(satCheckList);
+        urlAddon += '?the_date=';
+        urlAddon += $(selectDate).val();
+        for (var m = 0; m < satCheckList.length; m++){
+        // replace spaces with "+" and concatenate GET request for API
+            urlAddon += '&' + satCheckList[m].replace(/ /g,"+") +
+                        '=' + satCheckList[m].replace(/ /g,"+");
+        };
+        // create a cookie to store urlAddon for later use in /map page
+        var date = new Date();
+        // lifetime: 10 sec
+        date.setTime(date.getTime() + (3600 * 1000));
+        Cookies.set('urlAddon', urlAddon, { expires: date });
     });
-    document.addEventListener('mousemove', function(event){
-        globalX.innerHTML = event.screenX;
-        globalY.innerHTML = event.screenY;
-    });
 
+    urlAddon = Cookies.get('urlAddon');
+//    Cookies.remove('urlAddon');
+    if (mapDiv){
+        getSatHistory(urlAddon);
+
+
+        var globalX = document.querySelector('#globalX');
+        var globalY = document.querySelector('#globalY');
+        var localX = document.querySelector('#localX');
+        var localY = document.querySelector('#localY');
+
+        mapDiv.addEventListener('mousemove', function(event){
+           localX.innerHTML = event.clientX - mapDiv.offsetLeft;
+           localY.innerHTML = event.clientY - mapDiv.offsetTop;
+        });
+        document.addEventListener('mousemove', function(event){
+            globalX.innerHTML = event.screenX;
+            globalY.innerHTML = event.screenY;
+        });
+    };
 });
 
